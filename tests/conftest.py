73689d3d1a3123927994a2f2c548673f081b9015
Filip Chochół <nawijarka.wspolnota_0b@icloud.com>
2026-05-27 23:57:20 +0200

    Fix SSF_WEBHOOK_SECRET in test env setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ os.environ.setdefault("SSF_ISSUER", "https://idp.example.com/application/o/apple
 os.environ.setdefault("SSF_BASE_URL", "https://idp.example.com/shared-signals")
 os.environ.setdefault("SSF_ROOT_PATH", "/shared-signals")
 os.environ.setdefault("SSF_CONTAINER_PORT", "8000")
-os.environ.setdefault("AUTHENTIK_WEBHOOK_SECRET", "test_secret_min_32_chars_1234567890")
+os.environ.setdefault("SSF_WEBHOOK_SECRET", "test_secret_min_32_chars_1234567890")
 os.environ.setdefault("LOG_LEVEL", "INFO")
 os.environ.setdefault("SSF_DATABASE_PATH", str(TESTDATA / "ssf.db"))
 os.environ.setdefault("SSF_KEYS_DIR", str(TESTDATA / "keys"))
